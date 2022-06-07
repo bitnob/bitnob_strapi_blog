9c406fbd614785889ca9ae3bd38c3a87ae31e5cc
Houdini <nonsoamadi@aol.com>
2022-06-07 10:00:07 +0000

    :hammer: update post

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,13 +1,5 @@
 "use strict";
 
-/**  *  post controller
- */
-
-// const { createCoreController } = require('@strapi/strapi').factories;
-
-// module.exports = createCoreController('api::post.post');
-
-// path: ./src/api/post/controllers/post.js
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
@@ -20,11 +12,6 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
       populate: { primary_tag: true, primary_author: true },
     });
 
-    // console.log(entity);
-    // const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    // console.log(sanitizedEntity);
-
-    // return this.transformResponse(sanitizedEntity);
     return this.transformResponse(entity);
   },
 
@@ -38,28 +25,7 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
         },
       },
     });
-
-    // console.log(entity);
-    // const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    // console.log(sanitizedEntity);
-
-    // return this.transformResponse(sanitizedEntity);
     return this.transformResponse(entity);
   },
 }));
 
-// module.exports = createCoreController('api::post.post', ({ strapi }) =>  ({
-//   async findOne(ctx) {
-//     const {primary_tag, primary_author} = ctx.query;
-//     console.log(primary_tag, primary_author)
-
-//     const entity = await strapi.db.query('api::post.post').findMany({
-//       populate: ['primary-tag', 'primary-author'],
-//     });
-//     console.log(entity)
-//     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-//     console.log(sanitizedEntity)
-
-//     return this.transformResponse(sanitizedEntity);
-//   }
-// }));
