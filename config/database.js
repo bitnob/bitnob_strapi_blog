102f35c5d6216086f5fda15adf33f980168e58af
Houdini <nonsoamadi@aol.com>
2022-10-24 12:54:57 +0100

    (chore): fix conflict

--- config/database.js
+++ config/database.js
@@ -4,7 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      
     },
     debug: false,
   },
