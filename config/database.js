ec7305eb42646ec1fde85d4da47250a7250232c0
Houdini <nonsoamadi@aol.com>
2022-10-24 12:54:23 +0100

    (chore): fix conflict

--- config/database.js
+++ config/database.js
@@ -2,18 +2,10 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     debug: false,
-    pool: {
-      min: 0,
-      max: 10,
-      idleTimeoutMillis: 30000000,
-      createTimeoutMillis: 30000000,
-      acquireTimeoutMillis: 30000000,
-      propagateCreateError: false,
-    },
   },
 });
