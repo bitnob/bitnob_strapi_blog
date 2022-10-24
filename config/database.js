8fc3fa459e21e5260b5ddb3e858f8fba9a4e9b84
Houdini <nonsoamadi@aol.com>
2022-10-24 12:54:23 +0100

    (chore): fix conflict

--- config/database.js
+++ config/database.js
@@ -13,13 +13,5 @@ module.exports = ({ env }) => ({
       
     },
     debug: false,
-    pool: {
-      min: 0,
-      max: 10,
-      idleTimeoutMillis: 30000000,
-      createTimeoutMillis: 30000000,
-      acquireTimeoutMillis: 30000000,
-      propagateCreateError: false,
-    },
   },
 });
